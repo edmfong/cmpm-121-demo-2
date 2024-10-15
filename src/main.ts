492f2a4f54da9bbceae655eff0406ae50106455d
edmfong <97255519+edmfong@users.noreply.github.com>
2024-10-14 17:00:03 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,18 @@ const APP_NAME = "Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const h1: HTMLElement = document.createElement('h1');
+h1.textContent = 'Sketchpad';
+app.appendChild(h1);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
+
+const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+if (ctx) {
+  ctx.fillStyle = 'white'; // Set fill color
+  ctx.fillRect(0, 0, 256, 256);
+}
